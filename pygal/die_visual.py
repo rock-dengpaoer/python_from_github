0c5591f640a05b88c9de5c61c0d5e46df7c22c8f
rock-dengpaoer <xdt0916@qq.com>
2021-08-17 22:07:51 +0800

    8-17

--- pygal/die_visual.py
+++ pygal/die_visual.py
@@ -1,18 +1,24 @@
 from die import Die
 import pygal
 
-die = Die()
+die_1 = Die()
+die_2 = Die(10)
 
 results = []
 
-for roll_num in range(1000):
-    result = die.roll()
+# 投掷多次，并将结果存储到一个列表中
+for roll_num in range(50000):
+    result = die_1.roll() + die_2.roll()
     results.append(result)
 
 # print(results)
 
+# 分析结果
 frequencies = []
-for value in range(1, die.num_side + 1):
+
+max_result = die_1.num_side + die_2.num_side
+
+for value in range(2, max_result + 1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
@@ -21,10 +27,14 @@ for value in range(1, die.num_side + 1):
 # 对结果进行可视化
 hist = pygal.Bar()
 
-hist.title = "Results of rolling one D6 1000 times"
-hist.x_labels = ['1', '2', '3', '4', '5', '6']
-hist.x_title = "Result"
-hist.y_labels = "Frequency of Result"
+# hist.title = "Results of rolling one D6 1000 times"
+# hist.x_labels = ['1', '2', '3', '4', '5', '6']
 
-hist.add('D6', frequencies)
+hist.title = "Results of rollings  D6 and D10 dice 50000 times."
+hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16']
+
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+
+hist.add('D6 + D10', frequencies)
 hist.render_to_file('die_visual.svg')
